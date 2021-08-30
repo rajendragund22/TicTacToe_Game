9e0a534c9ec19c18808be8cfa4097863a54d851a
RajendraGund <rajendragund2015@gmail.com>
2021-08-30 17:24:05 +0530

    Added : Program for computer to choose and then side

--- src/com/bridgelabz/TicTacToeGame.java
+++ src/com/bridgelabz/TicTacToeGame.java
@@ -54,6 +54,12 @@ public class TicTacToeGame {
                     flag = 0;
                     return;
                 }
+                flag = computerCenterSide();
+                if (flag == 1) {
+                    turn++;
+                    flag = 0;
+                    return;
+                }
             }
         }
     }
@@ -249,6 +255,25 @@ public class TicTacToeGame {
         return flag;
 
     }
+    private static int computerCenterSide() {
+        if (element[5] != 'X' && element[5] != 'O') {
+            element[5] = computerMark;
+            System.out.println("Computer choice is '5'");
+            flag = 1;
+        } else {
+            int side[] = { 2, 6, 8, 4 };
+            for (int j = 0; j < 4; j++) {
+                if (element[side[j]] != 'X' && element[side[j]] != 'O') {
+                    element[side[j]] = computerMark;
+                    System.out.println("My choice is '" + side[j] + "'");
+                    flag = 1;
+                    break;
+                }
+            }
+        }
+        return flag;
+    }
 }
+
 
 
