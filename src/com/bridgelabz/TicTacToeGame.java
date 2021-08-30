6a978ef9117a4b651cfa8f4356b9973010c0fe2b
RajendraGund <rajendragund2015@gmail.com>
2021-08-30 15:16:08 +0530

    Added : Program for Player to choose a letter X or O

--- src/com/bridgelabz/TicTacToeGame.java
+++ src/com/bridgelabz/TicTacToeGame.java
@@ -1,11 +1,14 @@
 package com.bridgelabz;
-
+import java.util.Scanner;
 public class TicTacToeGame {
     private static char[] element;
+    static char userMark, computerMark;
+    static Scanner scan = new Scanner(System.in);
 
     public static void main(String[] args) {
         System.out.println("Welcome to the Tic Tac Toe Game !!!!!!");
         boardCreation();
+        choosingXorO();
     }
 
     private static void boardCreation() {
@@ -13,6 +16,22 @@ public class TicTacToeGame {
         for (int i = 1; i < 10; i++) {
             element[i] = ' ';
         }
-        System.out.println("Empty board created");
+}
+    private static void choosingXorO() {
+        System.out.println("Choose 1 for 'X' or Choose 2 for 'O' as your mark");
+        int option = scan.nextInt();
+        switch (option) {
+            case 1:
+                userMark = 'X';
+                computerMark = 'O';
+                break;
+            case 2:
+                userMark = 'O';
+                computerMark = 'X';
+                break;
+            default:
+                System.out.println("Your input is invalid");
+                choosingXorO();
+        }
     }
 }
