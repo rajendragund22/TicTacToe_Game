860bda296b1b5f6644673a1f2b304202093e1026
RajendraGund <rajendragund2015@gmail.com>
2021-08-30 15:24:42 +0530

    Added : Program for Display the Board

--- src/com/bridgelabz/TicTacToeGame.java
+++ src/com/bridgelabz/TicTacToeGame.java
@@ -1,5 +1,7 @@
 package com.bridgelabz;
+
 import java.util.Scanner;
+
 public class TicTacToeGame {
     private static char[] element;
     static char userMark, computerMark;
@@ -9,6 +11,7 @@ public class TicTacToeGame {
         System.out.println("Welcome to the Tic Tac Toe Game !!!!!!");
         boardCreation();
         choosingXorO();
+        currentBoard();
     }
 
     private static void boardCreation() {
@@ -16,7 +19,8 @@ public class TicTacToeGame {
         for (int i = 1; i < 10; i++) {
             element[i] = ' ';
         }
-}
+    }
+
     private static void choosingXorO() {
         System.out.println("Choose 1 for 'X' or Choose 2 for 'O' as your mark");
         int option = scan.nextInt();
@@ -33,5 +37,20 @@ public class TicTacToeGame {
                 System.out.println("Your input is invalid");
                 choosingXorO();
         }
+    }
+    private static void currentBoard() {
+        for (int i = 1; i < 10; i++) {
+            if (element[i] != 'X' && element[i] != 'O') {
+                element[i] = (char) (i + '0');
+            }
+        }
+        displayingBoard();
+}
+    private static void displayingBoard() {
+        System.out.println("\n  " + element[1] + " | " + element[2] + " | " + element[3] + " ");
+        System.out.println(" ----------- ");
+        System.out.println("  " + element[4] + " | " + element[5] + " | " + element[6] + " ");
+        System.out.println(" ----------- ");
+        System.out.println("  " + element[7] + " | " + element[8] + " | " + element[9] + " \n");
     }
 }
